b0f709c0791fb6dc983ae811d827c5fc568a641c
Bruno Jutkoski <bjutkoski@gmail.com>
2020-04-27 22:06:50 -0300

    3. Access Express request Object in NestJs

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res, Query } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('products')
 export class ProductsController {
   @Get()
-  findAll() {
-    return 'find all';
+  findAll(
+    @Req()
+    request: Request,
+    @Res()
+    response: Response,
+    @Query()
+    query,
+  ): Response {
+    console.log(query);
+    return response.json({ msg: 'find all' });
   }
 }
