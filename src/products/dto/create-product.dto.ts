ad48864996d67097687111307a49d74711e2b784
Bruno Jutkoski <bjutkoski@gmail.com>
2020-04-28 20:17:28 -0300

    26. Add Validations using ValidationPipe

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,16 @@
+import { IsNotEmpty, IsString, IsInt, IsNumber } from 'class-validator';
+
 export class CreateProductDTO {
+  @IsNotEmpty({ message: 'id deve ser informado' })
+  @IsString({ message: 'id deve ser uma string' })
   readonly id: string;
+  @IsNotEmpty()
+  @IsString()
   readonly name: string;
+  @IsNotEmpty()
+  @IsInt()
   readonly qty: number;
+  @IsNotEmpty()
+  @IsNumber()
   readonly price: number;
 }
