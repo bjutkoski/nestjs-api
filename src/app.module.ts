8b3c525bfd7b19c86501baefe3b6656a1044467b
Bruno Jutkoski <bjutkoski@gmail.com>
2020-04-28 20:38:12 -0300

    29. Creating a Custom Logging Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { ProductsController } from './products/products.controller';
 
 @Module({
   imports: [ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consume: MiddlewareConsumer) {
+    // consume.apply(LoggerMiddleware).forRoutes(ProductsController);
+    // consume
+    // .apply(LoggerMiddleware)
+    // .forRoutes({ path: 'products', method: RequestMethod.GET });
+    consume
+      .apply(LoggerMiddleware)
+      .exclude({ path: 'products', method: RequestMethod.GET })
+      .forRoutes(ProductsController);
+  }
+}
