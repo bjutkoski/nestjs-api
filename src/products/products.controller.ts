e6c2d04c23c20bec808f1a87802ab1f9578252be
Bruno Jutkoski <bjutkoski@gmail.com>
2020-04-28 23:52:46 -0300

    30. Creating HttpException Filter

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,12 +6,19 @@ import {
   Delete,
   Param,
   Body,
+  HttpException,
+  HttpStatus,
+  ForbiddenException,
+  NotFoundException,
+  UseFilters,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDTO } from './dto/create-product.dto';
 import { Product } from './interfaces/product.interface';
+import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
 
 @Controller('products')
+@UseFilters(HttpExceptionFilter)
 export class ProductsController {
   constructor(private productService: ProductsService) {}
 
@@ -37,6 +44,9 @@ export class ProductsController {
 
   @Delete(':id')
   async delete(@Param() params): Promise<Product[]> {
-    return this.productService.delete(params.id);
+    // return this.productService.delete(params.id);
+    // throw new HttpException('deu pau', HttpStatus.BAD_REQUEST);
+    // throw new ForbiddenException();
+    throw new NotFoundException('Não encontrado');
   }
 }
